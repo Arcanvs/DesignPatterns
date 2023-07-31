ad343f9e9374537f64a25ba082d200626a80772b
Jose <jose.osorio.herrera@gmail.com>
2023-07-31 00:15:23 -0400

    ejemplo de Factory Method

---
+++ src/FactoryMethod/index.ts
@@ -0,0 +1,48 @@
+// Interface comun para todos objetos
+interface Animal {
+    makeSound(): string;
+}
+
+// Creator, clase creadora Factory Method
+abstract class AnimalFactory {
+    public abstract createAnimal(): Animal;
+
+    public makeSound(): string {
+        const animal = this.createAnimal();
+        return `El animal hace ${animal.makeSound()}`;
+    }
+}
+
+// Concrete Creators, sobreescribe el Factory Method para retornar un tipo diferente
+class DogFactory extends AnimalFactory {
+    public createAnimal(): Animal {
+        return new Dog();
+    }
+}
+
+class FishFactory extends AnimalFactory {
+    public createAnimal(): Animal {
+        return new Fish();
+    }
+}
+
+// Concrete Products, diferentes implementaciones de la interface
+class Dog implements Animal {
+    public makeSound(): string {
+        return "Guau guau!!!"
+    }
+}
+
+class Fish implements Animal {
+    public makeSound(): string {
+        return "Glup glup!!!"
+    }
+}
+
+// Cliente
+function cliente(factory: AnimalFactory){
+    console.log(factory.makeSound());
+}
+
+cliente(new DogFactory());
+cliente(new FishFactory());
